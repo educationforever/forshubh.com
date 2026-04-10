3c8b9b0bb6c52cd1b3dc791a9bc47656f8c2d320
educational <Dinorexchris05@gmail.com>
2026-04-09 21:39:17 -0400

    Update uv.config.js

--- static/uv/uv.config.js
+++ static/uv/uv.config.js
@@ -1,9 +1,9 @@
 self.__uv$config = {
-    prefix: '/service/',
+    bare: 'https://bare.benrogu.li/', // Add this line if it's missing
+    prefix: '/uv/service/',
     encodeUrl: Ultraviolet.codec.xor.encode,
     decodeUrl: Ultraviolet.codec.xor.decode,
     handler: '/uv/uv.handler.js',
-    client: '/uv/uv.client.js',
     bundle: '/uv/uv.bundle.js',
     config: '/uv/uv.config.js',
     sw: '/uv/uv.sw.js',
